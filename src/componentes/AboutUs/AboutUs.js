03eae4932ad895770a2f69d553abd6c0c3b340c0
ondev <dfashion.corp@gmail.com>
2022-02-18 18:18:17 -0600

    Creacion del componente AboutUs y css

---
+++ src/componentes/AboutUs/AboutUs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+/**Import Estilos Css */
+import './AboutUs.css';
+
+
+const AboutUs = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AboutUs
