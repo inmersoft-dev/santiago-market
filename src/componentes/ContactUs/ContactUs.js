51fc36d0f1690961f94d026c0d7d1aa4db66c50a
Sito Nimbus <97991438+imenvyx@users.noreply.github.com>
2022-02-19 20:49:57 -0800

    ContactUs con Maquetado completo con degradado

--- src/componentes/ContactUs/ContactUs.js
+++ src/componentes/ContactUs/ContactUs.js
@@ -1,18 +1,43 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import Button from '@mui/material/Button';
 
 import FormControl from "@mui/material/FormControl";
+import { styled } from '@mui/material/styles';
+
+import sideImg from './../../assets/carrusel/3.jpg';
 
 /**Importando Estilos CSS */
 import "./ContactUs.css";
-import Button from '@mui/material/Button'
+
+const CssTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: '#fff',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: '#fff',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#fff',
+    },
+    '&:hover fieldset': {
+      borderColor: '#fff',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#fff',
+    },
+  },
+});
 
 const ContactUs = () => {
   return (
     <div className="contactUs-container">
       <div className="contactUs-form-container">
-        <Typography
+
+        <div className="text-container">
+          <Typography
           className="contactUs-form-bold"
           variant="h3"
           color="initial"
@@ -21,22 +46,25 @@ const ContactUs = () => {
         </Typography>
         <Typography
           className="contactUs-form-text"
-          variant="body1"
+          variant="h6"
           color="initial"
         >
           'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
           commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
           et magnis dis parturient montes, nascetur '
         </Typography>
+        </div>
+        
 
         <FormControl className="contactUs-form" variant="standard">
-          <TextField className="contactUs-form-items" 
+          <CssTextField className="contactUs-form-items" 
             id="nombre"
             label="Nombre y Apellidos"
             variant="outlined"
+            
           />
-          <TextField  className="contactUs-form-items" id="email" label="Correo electronico" variant="outlined" />
-          <TextField className="contactUs-form-items" 
+          <CssTextField  className="contactUs-form-items" id="email" label="Correo electronico" variant="outlined" />
+          <CssTextField className="contactUs-form-items" 
             id="multiline"
             label="Enviar un mensaje"
             multiline
@@ -44,12 +72,15 @@ const ContactUs = () => {
             defaultValue=""
           />
 
-          <Button  className="contactUs-form-items" className="contactUs-form-button" variant="contained">
-            Enviar
-          </Button>
+            <Button className="contactUs-form-button" /* color="secondary" */ variant="contained"sx={{ backgroundColor:'#7188df'}}>
+              Enviar
+            </Button>
         </FormControl>
       </div>
-      <div className="contactUs-img-container"></div>
+      <div className="contactUs-img-container">
+          <img className="sideImg" src={sideImg} alt=""/>
+          <div className="sideImg-fade"/>
+      </div>
     </div>
   );
 };
