75f932407bce86d1d2f33815ac870fc4e31d5f4c
Sito Nimbus <40501794+SitoNumbis@users.noreply.github.com>
2022-02-22 16:12:26 -0800

    Update About Us Component
    Update About Us Component

--- src/componentes/AboutUs/AboutUs.js
+++ src/componentes/AboutUs/AboutUs.js
@@ -30,7 +30,7 @@ const AboutUs = () => {
               <img className='aboutus-img1' src={about1} alt="" />
             </div>
             <div className='section2'>
-              <Typography className="aboutus-art-text" variant="h2" color="#fff"> ¿Qué es San en Santiago?</Typography>
+              <Typography className="aboutus-art-text" variant="h5" color="#fff"> ¿Qué es San en Santiago?</Typography>
 
               <Typography className="aboutus-art-text" variant="h6" color="#fff">
                 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
@@ -43,12 +43,20 @@ const AboutUs = () => {
           </div>
 
           <div className='aboutus-art-container'>
-            <Typography className="aboutus-art-text" variant="h6" color="#fff">
+
+            <div className='section3'>
+               <Typography className="aboutus-art-text" variant="h6" color="#fff">
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
             </Typography>
+            </div>
+
+            <div className='section4'>
+            <img className='aboutus-img2' src={about2} alt="" />  
+            </div>
+           
 
 
-            <img className='aboutus-img2' src={about2} alt="" />
+            
 
 
           </div>
