1d861493afd6286ac900c3b1b470a2d4a9d9d1f7
imenvyx <imdj9003@gmail.com>
2022-02-22 17:30:25 -0800

    Update About  Us Estructure

--- src/componentes/AboutUs/AboutUs.js
+++ src/componentes/AboutUs/AboutUs.js
@@ -30,9 +30,9 @@ const AboutUs = () => {
               <img className='aboutus-img1' src={about1} alt="" />
             </div>
             <div className='section2'>
-              <Typography className="aboutus-art-text" variant="h5" color="#fff"> ¿Qué es San en Santiago?</Typography>
+              <Typography className="aboutus-art-text1" variant="h5" color="#fff"> ¿Qué es San en Santiago?</Typography>
 
-              <Typography className="aboutus-art-text" variant="h6" color="#fff">
+              <Typography className="aboutus-art-text1" variant="h6" color="#fff">
                 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
               </Typography>
 
