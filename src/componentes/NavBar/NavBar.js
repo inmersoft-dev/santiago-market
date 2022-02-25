0e8820c1db75b0bd22e81544609533af5a9a2c7e
imenvyx <imdj9003@gmail.com>
2022-02-24 18:00:17 -0800

    Navbar and Header Update

--- src/componentes/NavBar/NavBar.js
+++ src/componentes/NavBar/NavBar.js
@@ -1,45 +1,96 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import SearchIcon from '@mui/icons-material/Search';
-import Typography from '@mui/material/Typography';
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import SearchIcon from "@mui/icons-material/Search";
+import Typography from "@mui/material/Typography";
 
 import "./NavBar.css";
 
 import Logo from "./../../assets/logo.svg";
 
-
-const sections =["Que es el San", "Santiago Shawsace","Programación","Cortografia", "Acreditaciones","Contacto"]
+const sections = [
+  "Que es el San",
+  "Santiago Shawsace",
+  "Programación",
+  "Cortografia",
+  "Acreditaciones",
+  "Contacto",
+];
 
 export default function NavBar() {
-  return (
-    <div className='navbar-container'>
-        {/* Section izquierda del la Barra de Navegacion */}
-       <div className='navbar-left'>
-            <img className="navbar-left-log" src={Logo} alt=''/>
-       </div>
-       {/* Section Central del la Barra de Navegacion */}
-       <div className='navbar-center'>
-       {
-         sections.map((section)=>
-            (
-              <div className="navbar-center-link">
-                <Typography  className="navbar-center-link-text"  key={section} variant="body1" color="#fff" sx={{textAlign:'center'}}> {section}</Typography>   
-              </div>            
-            
-            ))
-        }    
+  const [fondo, handleFondo] = useState(false);
 
-         
-       </div>
-       {/* Section Derecha del la Barra de Navegacion */}
-       <div className='navbar-rigth'>
-           <div className="navbar-rigth-search">
-              <input  type="text"/>
-              <SearchIcon fontSize="large" style={{color:'#fff'}} />              
-           </div>
-           <Button className="navbar-button-rigth" variant="contained">Suscribir</Button>
-       </div>
-      
+  const [show, handleShow] = useState(true);
+
+  const showControl = () => {
+    if (show) {
+      handleShow(false);
+    }
+  };
+  const showtime = () =>
+    setTimeout(() => {
+      showControl();
+      console.log(show);
+    }, 4000);
+
+  const transitionNavBar = () => {
+    console.log(window.scrollY);
+    if (window.scrollY > 100) {
+      handleFondo(true);
+      showtime();
+    } else {
+      handleFondo(false);
+    }
+  };
+
+  /* const hideNavBar=()=>{
+
+}; */
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => {
+      window.removeEventListener("scroll", transitionNavBar);
+      clearTimeout(showtime);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`navbar-container ${fondo && "nav-painted"} ${
+        !show && "nav-noShow"
+      }`}
+    >
+      {/* Section izquierda del la Barra de Navegacion */}
+      <div className="navbar-left">
+        <img className="navbar-left-log" src={Logo} alt="" />
+      </div>
+      {/* Section Central del la Barra de Navegacion */}
+      <div className="navbar-center">
+        {sections.map((section) => (
+          <div className="navbar-center-link">
+            <Typography
+              className="navbar-center-link-text"
+              key={section}
+              variant="body1"
+              color="#fff"
+              sx={{ textAlign: "center" }}
+            >
+              {" "}
+              {section}
+            </Typography>
+          </div>
+        ))}
+      </div>
+      {/* Section Derecha del la Barra de Navegacion */}
+      <div className="navbar-rigth">
+        <div className="navbar-rigth-search">
+          <input type="text" />
+          <SearchIcon fontSize="large" style={{ color: "#fff" }} />
+        </div>
+        <Button className="navbar-button-rigth" variant="contained">
+          Suscribir
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
--- src/componentes/theme/Theme.js
+++ src/componentes/theme/Theme.js
@@ -1,14 +1,13 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
-    palette: {
-        type: 'light',
-        primary: {          
-          main: '#dd1a1e',  /* Rojo */
-        },
-        secondary: {
-          
-          main: '#0c2372',  /** Azul */
-        },
-      },
+  palette: {
+    type: "light",
+    primary: {
+      main: "#dd1a1e" /* Rojo */,
+    },
+    secondary: {
+      main: "#0c2372" /** Azul */,
+    },
+  },
 });
