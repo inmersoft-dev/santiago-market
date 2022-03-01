275f73e932d1bdef4c48d8168df5a36e230a095e
imenvyx <imdj9003@gmail.com>
2022-02-28 16:40:17 -0800

    AboutUs view with Header component

---
+++ src/views/AboutUs/AboutUS.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../componentes/Header/Header";
+
+import "./AboutUs.css";
+
+const AboutUS = () => {
+  return (
+    <>
+      <Header />
+      
+      {/* Description */}
+      {/* InscriptionDescription */}
+      {/* InscriptionRequest */}
+    </>
+  );
+};
+
+export default AboutUS;
