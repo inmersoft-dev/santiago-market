bf52873c4248e97a666f9b0cebc2503d38613975
imenvyx <imdj9003@gmail.com>
2022-03-04 16:45:09 -0800

    SectionCarrusel update maquetacion

--- src/componentes/CarruselSimple/CarruselSimple.js
+++ src/componentes/CarruselSimple/CarruselSimple.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 
+import CarruselItem from "./../Carruseltem/CarruselItem";
 
-import CarruselItem from './../Carruseltem/CarruselItem';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-/* 
-import "./slick.min.css"; 
-import "./slick-theme.min.css"; */
+/* import './slick.min.css';
+import './slick-theme.min.css'; */
+
 
 /* function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -33,52 +35,91 @@ function SamplePrevArrow(props) {
 export default class CarruselSimple extends Component {
   render() {
     const settings = {
-      className: "",
+      className: "carruselSimple-slide",
       dots: true,
       infinite: true,
-      slidesToShow: 4,
+      slidesToShow: 3,
       slidesToScroll: 1,
       adaptiveHeight: true,
-      nextArrow: <button type="button" class="slick-next">Next</button> ,
-      prevArrow: <button type="button" class="slick-prev">Previous</button>,      
-      
+      /* centerPadding: 100, */
+      nextArrow: (
+        <button type="button" class="slick-next">
+          Next
+        </button>
+      ),
+      prevArrow: (
+        <button type="button" class="slick-prev">
+          Previous
+        </button>
+      ),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+      useCSS: true
     };
     return (
-      <div className="carruselSimple-container">       
-        <Slider {...settings}>
-          <div>
-            <CarruselItem/>
+      <div className="carruselSimple-container">
+        <Slider
+          {...settings}
+          style={{           
+            margin: "20px",
+          }}
+        >
+          <div className="Slide-container">
+            <CarruselItem />
           </div>
-          <div>
-            <CarruselItem/>
+          <div className="Slide-container">
+            <CarruselItem />
           </div>
-          <div>
-            <CarruselItem/>
-          </div>
-
-          <div>
-            <CarruselItem/>
-          </div>
-          <div>
-            <CarruselItem/>
-          </div>
-          <div>
-            <CarruselItem/>
-          </div>
-          <div>
-            <CarruselItem/>
-          </div>
-          <div>
-            <CarruselItem/>
+          <div className="Slide-container">
+            <CarruselItem />
           </div>
 
-          <div>
-            <CarruselItem/>
+          <div className="Slide-container">
+            <CarruselItem />
           </div>
-          <div>
-            <CarruselItem/>
+          <div className="Slide-container">
+            <CarruselItem />
           </div>
-          
+          <div className="Slide-container">
+            <CarruselItem />
+          </div>
+          <div className="Slide-container">
+            <CarruselItem />
+          </div>
+          <div className="Slide-container">
+            <CarruselItem />
+          </div>
+
+          <div className="Slide-container">
+            <CarruselItem />
+          </div>
+          <div className="Slide-container">
+            <CarruselItem />
+          </div>
         </Slider>
       </div>
     );
