06c7de946a7d49157b7829520e8869fb28f773fe
imenvyx <imdj9003@gmail.com>
2022-03-04 20:07:47 -0800

    Update Masonry
    Update Masonry

--- src/componentes/Masonry/Masonry.js
+++ src/componentes/Masonry/Masonry.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Box from '@mui/material/Box';
+
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
@@ -9,6 +9,7 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 
 import './Masonry.css';
+import { Box } from '@mui/material';
 
 const itemData = [
     {
@@ -35,41 +36,18 @@ const itemData = [
       img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
       title: 'Chairs',
     },
-    {
-      img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-      title: 'Laptop',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-      title: 'Doors',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-      title: 'Coffee',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-      title: 'Storage',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-      title: 'Candle',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-      title: 'Coffee table',
-    },
+    
   ];
 
 const Masonry = () => {
   return (
-    <Box className='masonry-container' sx={{ width: 1200, height: 1000, overflowY: 'scroll' }}>
-    <ImageList variant="masonry" cols={3} gap={3} >
+    <Box className='masonry-container' sx={{ width: 1200, height: 900, overflowY: 'scroll' }}>
+    <ImageList variant="masonry" cols={2} gap={1} >
       {itemData.map((item) => (
         <ImageListItem key={item.img}>
           <img
-            src={`${item.img}?w=895&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=895&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.img}?w=495&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=595&fit=crop&auto=format&dpr=2 2x`}
             alt={item.title}
             loading="lazy"
           />
