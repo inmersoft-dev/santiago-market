69b82764b8a859f8145672af9a6850c23378f31b
imenvyx <imdj9003@gmail.com>
2022-03-05 17:46:28 -0800

    Update SimpleHeader

--- src/componentes/SimpleHeader/SimpleHeader.js
+++ src/componentes/SimpleHeader/SimpleHeader.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import NavBar from '../NavBar/NavBar';
+import React from "react";
+import NavBar from "../NavBar/NavBar";
 
-import './SimpleHeader.css';
-import Typography from '@mui/material/Typography'
-
+import "./SimpleHeader.css";
+import Typography from "@mui/material/Typography";
 
 const SimpleHeader = () => {
   return (
-    <div className='simpleHeader-container'>
-        <NavBar/>
-        
-        <Typography className='simpleHeader-text' variant="h5" color="#fff">
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur '
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur '
-            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur '
+    <div className="simpleHeader-container">
+      <NavBar />
+
+      <div className="text-container">
+        <Typography className="simpleHeader-text" variant="h5" color="#fff">
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ' 'Lorem ipsum dolor sit
+          amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+          Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
+          montes, nascetur ' 'Lorem ipsum dolor sit amet, consectetuer
+          adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
+          sociis natoque penatibus et magnis dis parturient montes, nascetur '
         </Typography>
-        
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SimpleHeader;
