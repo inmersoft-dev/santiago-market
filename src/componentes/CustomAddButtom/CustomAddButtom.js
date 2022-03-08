1bb8ebb4e4155e4eb15b3071c43f67498599ca24
ImEnvyx <imdj9003@gmail.com>
2022-03-08 14:14:05 -0800

    CustomAddButtom and Masonry
    Create CustomAddButtom, update Masonry  and ImgCard

---
+++ src/componentes/CustomAddButtom/CustomAddButtom.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+
+import './CustomAddButtom.css'
+
+const CustomAddButtom = () => {
+  return (
+    <div className="imgCard-buttom">
+        <AddCircleIcon fontSize="large"/>
+      </div>
+  )
+}
+
+export default CustomAddButtom
