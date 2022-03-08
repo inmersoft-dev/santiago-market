3393c7e08428abc27c4925bfe2f6a7db6d58a719
imenvyx <imdj9003@gmail.com>
2022-03-08 08:56:27 -0800

    header styles

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -10,7 +10,8 @@ import banner from "./../../assets/banner.jpg";
 
 const contenido = [
   "Explora Santiago de los Caballeros en búsqueda de la oportunidad",
-  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.",
+  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. ",
+  "Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.",
 ];
 
 function Header() {
@@ -28,18 +29,32 @@ function Header() {
       <NavBar />
       <div className="section-container">
         <div className="banner-section">
-          <div className='banner-section-contenido1'>
-            <Typography variant="h4" color="#fff" sx={{ fontWeight:'600'}}>
+          <div className="banner-section-contenido1">
+            <Typography variant="h4" color="#fff" sx={{ fontWeight: "600" }}>
               {contenido[0]}
             </Typography>
           </div>
-          <div className='banner-section-contenido2'>
-
-            <Typography variant="body2" color="#fff"   sx={{
-             padding: '14px', 
-             /* lineHeight:'40px' */
-            }}>
+          <div className="banner-section-contenido2">
+            <div></div>
+            <Typography
+              variant="body2"
+              color="#fff"
+              sx={{
+                padding: "14px",
+                /* lineHeight:'40px' */
+              }}
+            >
               {contenido[1]}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="#fff"
+              sx={{
+                padding: "14px",
+                /* lineHeight:'40px' */
+              }}
+            >
+              {contenido[2]}
             </Typography>
           </div>
         </div>
