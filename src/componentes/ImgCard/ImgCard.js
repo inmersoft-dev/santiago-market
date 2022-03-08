9a1889b1fb488c1bb3b7b5e5690460306d1bc96f
ImEnvyx <imdj9003@gmail.com>
2022-03-08 14:40:03 -0800

    Update ImgCard

--- src/componentes/ImgCard/ImgCard.js
+++ src/componentes/ImgCard/ImgCard.js
@@ -1,15 +1,25 @@
+import { IconButton } from "@mui/material";
 import React from "react";
 import CustomAddButtom from "../CustomAddButtom/CustomAddButtom";
 
-
-
 import "./ImgCard.css";
 
-const ImgCard = ({img}) => {
+const ImgCard = ({ img }) => {
   return (
-    <div className="imgCard-container">
-      <img clasname="imgCard-img" src={img} alt="" />
-      <CustomAddButtom/>
+    <div
+      className="imgCard-container"
+      style={{
+        backgroundImage: `url(${img})`,
+      }}
+    >
+      <IconButton
+        
+        sx={{ marginRight: 3,
+        marginBottom:3 }}
+        
+      >
+        <CustomAddButtom />
+      </IconButton>
     </div>
   );
 };
