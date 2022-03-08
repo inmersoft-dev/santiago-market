e9d66d4504d0d0634fcc6b29fc9e0cb31c111b69
imenvyx <imdj9003@gmail.com>
2022-03-08 08:59:07 -0800

    social icons

--- src/componentes/Icons/Icons.js
+++ src/componentes/Icons/Icons.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
-import EmailIcon from '@mui/icons-material/Email';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import SvgIcon from '@mui/material/SvgIcon';
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import SvgIcon from "@mui/material/SvgIcon";
 
 /**Importando Estilo CSS */
-import './Icons.css';
+import "./Icons.css";
 
-const socialNet = [TwitterIcon,FacebookOutlinedIcon,EmailIcon,InstagramIcon,WhatsAppIcon]
+const socialNet = [
+  TwitterIcon,
+  FacebookOutlinedIcon,
+  EmailIcon,
+  InstagramIcon,
+  WhatsAppIcon,
+];
 
-function Icons({color}) {
+function Icons({ color }) {
   return (
     <div className="socialNetwork">
-     {
-         socialNet.map((item, index)=>{
-
-            return <div className='socialNetwork-container'>
-              <SvgIcon  className="socialNetwork-icon" key={index} component={item} fontSize="medium" sx={{color: {color}}}/>
-              </div>
-            
-         })
-     }
+      {socialNet.map((item, index) => {
+        return (
+          <div className="socialNetwork-container">
+            <SvgIcon
+              className="socialNetwork-icon"
+              key={index}
+              component={item}
+              fontSize="medium"
+              sx={{ color: { color } }}
+            />
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default Icons;
