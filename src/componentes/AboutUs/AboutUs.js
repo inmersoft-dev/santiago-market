eb5d0db210b5c01dc9233acef9c5d532cc73223c
imenvyx <imdj9003@gmail.com>
2022-03-08 17:03:33 -0800

    about master to sito

--- src/componentes/AboutUs/AboutUs.js
+++ src/componentes/AboutUs/AboutUs.js
@@ -25,7 +25,15 @@ const AboutUs = () => {
       >
         <div className="aboutUs-container">
           <div className="aboutus-imgs-container">
-            <img className="aboutus-img1" src={about1} alt="" />
+            <div className="section1"
+            style={{
+              backgroundImage:`url(${about1})`,
+              backgroundSize:'cover',
+              backgroundPosition:'center center'
+            }}
+            >
+            {/*   <img className="aboutus-img1" src={about1} alt="" /> */}
+            </div>
             <div className="section2">
               <Typography
                 className="aboutus-art-text1"
