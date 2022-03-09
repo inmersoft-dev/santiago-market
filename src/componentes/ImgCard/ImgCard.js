7fcbbdeb53bd3a8917e1238f822b2135b94fa8ae
imenvyx <imdj9003@gmail.com>
2022-03-09 09:49:31 -0800

    hover on image card

--- src/componentes/ImgCard/ImgCard.js
+++ src/componentes/ImgCard/ImgCard.js
@@ -1,18 +1,37 @@
 import React from "react";
 import CustomAddButton from "../CustomAddButton/CustomAddButton";
 
+import PropTypes from "prop-types";
+
 import "./ImgCard.css";
 
-const ImgCard = ({ img }) => {
+const ImgCard = ({ img, onMouseEnter, onMouseLeave }) => {
   return (
     <div className="imgCard-container">
-      <img clasname="imgCard-img" src={img} alt="" />
+      <img
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        clasname="imgCard-img"
+        src={img}
+        alt=""
+      />
 
       <div className="customAddbutton-container">
         <CustomAddButton padding={5} fontSize={15} />
       </div>
     </div>
   );
+};
+
+ImgCard.defaultProps = {
+  onMouseEnter: null,
+  onMouseLeave: null,
+};
+
+ImgCard.propTypes = {
+  img: PropTypes.string.isRequired,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func,
 };
 
 export default ImgCard;
--- src/componentes/Workshops/Workshop.js
+++ src/componentes/Workshops/Workshop.js
@@ -10,53 +10,63 @@ import img3 from "./../../assets/masonry/5.jpg";
 
 const Workshop = () => {
   const [imgSelect, SetimgSelect] = useState({ img1 });
+  const [opacity, setOpacity] = useState(0);
 
   const mouseImg = (e) => {
-    console.log(e.target.currentSrc);
-    SetimgSelect(e.target.currentSrc);
+    setOpacity(0);
+    setTimeout(() => {
+      SetimgSelect(e.target.currentSrc);
+      setOpacity(1);
+    }, 205);
   };
 
   const unMouseImg = (e) => {
-    SetimgSelect(`#dd1a1e`);
+    setOpacity(0);
   };
 
   return (
-    <div
-      className="workShop-container"
-      style={{
-        backgroundImage: `url(${imgSelect})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-        backgroundColor: "#dd1a1e",
-        animation: "all 200ms ease",
-      }}
-    >
-      <div className="workShop-right">
-        <div className="colum-1">
-          <div onMouseEnter={mouseImg} onMouseLeave={unMouseImg}>
-            <ImgCard img={img1} />
-          </div>
+    <div>
+      <div
+        className="workShop-container"
+        style={{
+          opacity,
+          backgroundImage: `url(${imgSelect})`,
+        }}
+      ></div>
+      <div className="workShop-backgroupd-container">
+        <div className="workShop-right">
+          <div className="colum-1">
+            <ImgCard
+              img={img1}
+              onMouseEnter={mouseImg}
+              onMouseLeave={unMouseImg}
+            />
 
-          <div onMouseEnter={mouseImg} onMouseLeave={unMouseImg}>
-            <ImgCard img={img2} />
+            <ImgCard
+              img={img2}
+              onMouseEnter={mouseImg}
+              onMouseLeave={unMouseImg}
+            />
+          </div>
+          <div className="colum-2">
+            <ImgCard
+              img={img3}
+              onMouseEnter={mouseImg}
+              onMouseLeave={unMouseImg}
+            />
           </div>
         </div>
-        <div className="colum-2">
-          <div onMouseEnter={mouseImg} onMouseLeave={unMouseImg}>
-            <ImgCard img={img3} />
-          </div>
-        </div>
-      </div>
-      <div className="workShop-lefth">
-        <Typography className="workShop-title" variant="h3" color="#fff">
-          Title of Section
-        </Typography>
+        <div className="workShop-left">
+          <Typography className="workShop-title" variant="h3" color="#fff">
+            Title of Section
+          </Typography>
 
-        <Typography className="workShop-text" variant="h6" color="#fff">
-          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-          et magnis dis parturient montes, nascetur '
-        </Typography>
+          <Typography className="workShop-text" variant="h6" color="#fff">
+            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur '
+          </Typography>
+        </div>
       </div>
     </div>
   );
