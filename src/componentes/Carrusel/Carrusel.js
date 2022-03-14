810da2858d11c8cc94d2b68f035a9c351967393a
sitonumbis <sito8943@gmail.com>
2022-03-14 10:39:20 -0700

    revisa el carrusel

--- src/componentes/Carrusel/Carrusel.js
+++ src/componentes/Carrusel/Carrusel.js
@@ -3,6 +3,10 @@ import Typography from "@mui/material/Typography";
 
 /**Importando Estilos CSS */
 import "./Carrusel.css";
+/* import banner1 from "./../../assets/carrusel/para slider/1.jpg";
+import banner2 from "./../../assets/carrusel/para slider/2.jpg";
+import banner3 from "./../../assets/carrusel/para slider/3.jpg";
+import banner4 from "./../../assets/carrusel/para slider/4.jpg"; */
 
 import carrusel1 from "./../../assets/carrusel/1.jpg";
 import carrusel2 from "./../../assets/carrusel/2.jpg";
@@ -14,68 +18,180 @@ import icono2 from "./../../assets/carrusel/icons/Grupo 16.svg";
 import icono3 from "./../../assets/carrusel/icons/Grupo 18.svg";
 import icono4 from "./../../assets/carrusel/icons/Grupo 20.svg";
 import { dark } from "../../assets/theme/Theme";
-import { style } from "@mui/system";
 
 const slide = [carrusel1, carrusel2, carrusel3, carrusel4];
+
+/* const sliderfondo = [banner1, banner2, banner3, banner4]; */
 
 const icons = [icono1, icono2, icono3, icono4];
 
 function Carrusel() {
   const [photoToggled, setPhotoToggled] = useState(-1);
+  const [outIn, setOutIn] = useState(false);
 
   const toggleHover = (e) => {
-    setPhotoToggled(Number(e.target.id.substring(1)));
+    const { id } = e.target;
+    if (id.indexOf("i") !== -1) {
+      // if enters in an image
+      if (Number(id.substring(1)) !== photoToggled) {
+        setPhotoToggled(Number(id.substring(1)));
+        if (outIn) transition(false);
+        transition(true);
+      }
+    }
+  };
+
+  const transition = (value) => {
+    setTimeout(() => {
+      setOutIn(value);
+    }, 100);
   };
 
   const unToggleHover = (e) => {
-    setPhotoToggled(-1);
+    const { id } = e.target;
+    console.log(id);
+    if (id.indexOf("i") !== -1) {
+      setPhotoToggled(-1);
+      setOutIn(false);
+    }
   };
 
   return (
-    <div
-      className="container-carrusel"
-      style={{ background: dark.palette.primary.main }}
-    >
-      <div className="carrusel-full" style={{ position: "absolute" }}>
-        {slide.map((item, index) => {
-          return (
+    <>
+      {
+      /*<div
+        className="container-carrusel"
+        style={{ background: dark.palette.primary.main }}
+      >
+        <div className="carrusel-full" style={{ position: "absolute" }}>
+          {photoToggled !== -1 && (
             <img
-              key={`image${index}`}
-              src={item}
+              src={slide[photoToggled]}
               alt="img"
               style={{
-                width:
-                  photoToggled !== -1
-                    ? photoToggled === index
-                      ? "default"
-                      : 0
-                    : "25%",
-                flex: photoToggled === index ? 1 : "none",
+                flex: "1",
+                opacity: photoToggled !== -1 ? 1 : 0,
               }}
             />
-          );
-        })}
-      </div>
-      <div className="carrusel-full">
-        {slide.map((item, index) => {
-          return (
-            <div key={index} className="carrusel-item">
-              <div
+          )}
+        </div>
+        <div
+          className="carrusel-full"
+          style={{
+            position: "absolute",
+            background: "aliceblue",
+            transition: "all 400ms ease",
+            opacity: !outIn ? 1 : 0,
+          }}
+        ></div>
+        <div className="carrusel-full" style={{ position: "absolute" }}>
+          {slide.map((item, index) => {
+            return (
+              <img
                 onMouseEnter={toggleHover}
                 onMouseLeave={unToggleHover}
+                key={`image${index}`}
+                id={`i${index}`}
+                src={item}
+                alt="img"
                 style={{
-                  zIndex: 1,
-                  display:
-                    photoToggled !== -1
-                      ? photoToggled === index
-                        ? "default"
-                        : "none"
-                      : "flex",
+                  width: "25%",
+                  flex: "none",
+                  opacity: photoToggled !== -1 ? 0 : 1,
                 }}
+              />
+            );
+          })}
+        </div>
+        <div className="carrusel-full">
+          {slide.map((item, index) => {
+            return (
+              <div key={index} className="carrusel-item">
+                <div
+                  onMouseEnter={toggleHover}
+                  onMouseLeave={null}
+                  style={{ zIndex: 1 }}
+                  id={`d${index}`}
+                  className="carrusel-column"
+                >
+                  <div className="carrusel-icon-item" id={`c${index}`}>
+                    <img src={icons[index]} alt="" id={`i${index}`} />
+                  </div>
+                  <Typography
+                    className="section"
+                    variant="h4"
+                    color="#fff"
+                    sx={{
+                      position: "relative",
+                      textTransform: "uppercase",
+                      marginTop: "84px",
+                    }}
+                    id={`t${index}`}
+                  >
+                    {" "}
+                    Prueba
+                  </Typography>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div> */
+      }
+      {/* codigo de sito */}
+      <div style={{ display: "flex", height: "80vh" }}>
+        <div className="carrusel-full" style={{ position: "absolute" }}>
+          {photoToggled !== -1 && (
+            <img
+              src={slide[photoToggled]}
+              alt="img"
+              style={{
+                flex: "1",
+                opacity: photoToggled !== -1 ? 1 : 0,
+              }}
+            />
+          )}
+        </div>
+        <div
+          className="carrusel-full"
+          style={{
+            position: "absolute",
+            background: "aliceblue",
+            transition: "all 400ms ease",
+            opacity: !outIn ? 1 : 0,
+          }}
+        ></div>
+        {slide.map((item, index) => {
+          return (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "25%",
+                zIndex: 99,
+              }}
+              onMouseEnter={toggleHover}
+              onMouseLeave={unToggleHover}
+            >
+              <img
+                id={`i${index}`}
+                src={item}
+                alt="img"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  flex: "none",
+                  opacity: photoToggled !== -1 ? 0 : 1,
+                }}
+              />
+              <div
+                style={{ zIndex: 1, position: "absolute" }}
                 id={`d${index}`}
                 className="carrusel-column"
               >
-                <div className="carrusel-icon-item" id={`c${index}`} style={{}}>
+                <div className="carrusel-icon-item" id={`c${index}`}>
                   <img src={icons[index]} alt="" id={`i${index}`} />
                 </div>
                 <Typography
@@ -97,7 +213,7 @@ function Carrusel() {
           );
         })}
       </div>
-    </div>
+    </>
   );
 }
 
