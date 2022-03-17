212c256114bba2894481adbc8577a05f3a2143e4
ImEnvyx <imdj9003@gmail.com>
2022-03-16 18:48:18 -0700

    Description Responsive Update

--- src/views/AboutUs/AboutUS.js
+++ src/views/AboutUs/AboutUS.js
@@ -17,7 +17,7 @@ const AboutUS = () => {
   return (
     <>
       <NavBar />
-      <Header img={footerImg} contenido={contenido} margin={330} />
+      <Header img={footerImg} contenido={contenido} margin={330} display="flex" />
       <Icons color="#e8eaf6" />
       <Description />
       <DescriptionInscription />
