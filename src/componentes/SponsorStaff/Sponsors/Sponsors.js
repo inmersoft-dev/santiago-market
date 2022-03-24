d9428c36ccecde4d3554a0bff8e898be1f5f333e
imenvyx <97991438+imenvyx@users.noreply.github.com>
2022-03-24 12:31:47 -0700

    Carrusel Sponsor Update

--- src/componentes/SponsorStaff/Sponsors/Sponsors.js
+++ src/componentes/SponsorStaff/Sponsors/Sponsors.js
@@ -1,15 +1,51 @@
 import React from "react";
-import "./Sponsors.css";
+
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import Sponsor from "./Sponsor/Sponsor";
 import sponsorImg from "./../../../assets/sponsor.png";
-import Slideshow from "../../Slideshow/Slideshow";
+/* import Slideshow from "../../Slideshow/Slideshow"; */
 import { Box } from "@mui/material";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Sponsors.css";
 const sponsors = [sponsorImg, sponsorImg, sponsorImg, sponsorImg, sponsorImg];
 
 const Sponsors = () => {
+
+  const settings = {
+    className: "carruselSponsors-slide",
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    centerMode: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnFocus: true,
+    mobileFirst: true,
+    fade: false,
+    cssEase: 'linear',
+    responsive: [
+
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+          variableWidth: false,
+          adaptiveHeight: false,
+        },
+      },
+
+    ],
+    useCSS: true,
+  };
   return (
     <div className="sponsors-container">
       <div className="sponsors-title-container">
@@ -18,13 +54,23 @@ const Sponsors = () => {
         </Typography>
       </div>
 
-      <Box className="sponsors-logos" style={{display: {md:"none" , lg:"flex"} }}>
+      <Box className="sponsors-logos" style={{ display: { md: "none", lg: "flex" } }}>
         {sponsors.map((item, index) => (
           <Sponsor sponsor={sponsorImg} key={index} />
         ))}
       </Box>
       <Box className="slideshow-content">
-        <Slideshow />
+        {/* <Slideshow /> */}
+
+        <Slider {...settings}>
+          {sponsors.map((sponsor, i) => {
+            return (
+              <div key={`div${i}`} className="sponsorSlide-container">
+                <Sponsor sponsor={sponsor} key={i} />
+              </div>
+            );
+          })}
+        </Slider>
       </Box>
 
       <Divider style={{ borderColor: "#fff" }} />
